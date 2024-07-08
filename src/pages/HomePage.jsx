2404f251695502a8e9df0f990132da11bcdf5609
Bagus Viki Amalindo <vikiamalindo7@gmail.com>
2024-07-08 21:33:52 +0700

    edit design home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Heading, Text, Button, Image, IconButton, Link, Grid, GridItem } from '@chakra-ui/react';
+import { Box, Flex, Button, IconButton, Link } from '@chakra-ui/react';
 import { Link as ScrollLink, Element } from 'react-scroll';
 import { FaWhatsapp } from 'react-icons/fa';
 import { motion } from 'framer-motion';
@@ -7,7 +7,7 @@ import About from '../components/About';
 import ProductsProses from '../components/ProductsProses';
 import Products from "../components/Products";
 import Assesments from '../components/Assesments';
-import fotoDaun from '../assets/foto_daun.jpg';
+import vndceper from '../assets/vndceper.jpg';
 
 const HomePage = () => {
   return (
@@ -33,51 +33,30 @@ const HomePage = () => {
               left="0"
               right="0"
               bottom="0"
-              backgroundImage={`url(${fotoDaun})`}
-              filter="blur(8px)"
+              width="100%"
+              height="100%"
+              backgroundImage={`url(${vndceper})`}
+              backgroundSize="contain"  // Bisa juga "contain" jika ingin sesuai dengan konten
+              backgroundPosition="center"
+              backgroundRepeat="no-repeat"
               zIndex="-1"
             ></Box>
 
-            <Grid
-              templateColumns={{ base: '1fr', md: '1fr 2fr' }}
-              gap={4}
-              alignItems="center"
-              width="100%"
-            >
-              <GridItem>
-                <Image 
-                  src="/image/foto_awal.png"
-                  alt="VND Ceper Industries"
-                  boxSize={{ base: '300px', md: '400px' }}
-                  objectFit="cover"
-                  borderRadius={10}
-                  width="100%"
-                  as={motion.img}
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.6 }}
-                />
-              </GridItem>
-              <GridItem>
-                <Box textAlign="left" p={4} as={motion.div}
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.6 }}
+            <Box position="relative" zIndex="1">
+              <ScrollLink to="about" smooth={true} duration={500}>
+                <Button
+                  colorScheme="white"
+                  size="lg"
+                  textColor="black"
+                  border="2px solid black"
+                  padding="10px 20px"
+                  borderRadius="md"
+                  marginTop="60%"
                 >
-                  <Heading as="h1" size="2xl" mb={4} fontFamily="'Emblema One', cursive" textColor="white">
-                    Selamat Datang di VND Ceper Industries
-                  </Heading>
-                  <Text fontSize="lg" mb={6} textColor="white">
-                    Solusi terbaik untuk kebutuhan pakaian jadi Anda.
-                  </Text>
-                  <ScrollLink to="about" smooth={true} duration={500}>
-                    <Button colorScheme="teal" size="lg">
-                      Pelajari Lebih Lanjut
-                    </Button>
-                  </ScrollLink>
-                </Box>
-              </GridItem>
-            </Grid>
+                  Pelajari Lebih Lanjut
+                </Button>
+              </ScrollLink>
+            </Box>
           </Flex>
         </Box>
       </Element>
